a0b33e2daff7b8ec2bb3ead4572a21a50c92ef47
Niran Babalola <niran@niran.org>
2011-06-24 12:59:06 -0500

    Add tests for #16326

--- tests/regressiontests/templates/response.py
+++ tests/regressiontests/templates/response.py
@@ -200,6 +200,19 @@ class SimpleTemplateResponseTest(BaseTemplateResponseTest):
             with self.assertRaises(DiscardedAttributeError) as cm:
                 getattr(unpickled_response, attr)
 
+    def test_repickling(self):
+        response = SimpleTemplateResponse('first/test.html', {
+                'value': 123,
+                'fn': datetime.now,
+            })
+        self.assertRaises(ContentNotRenderedError,
+                          pickle.dumps, response)
+
+        response.render()
+        pickled_response = pickle.dumps(response)
+        unpickled_response = pickle.loads(pickled_response)
+        repickled_response = pickle.dumps(unpickled_response)
+
 class TemplateResponseTest(BaseTemplateResponseTest):
 
     def _response(self, template='foo', *args, **kwargs):
@@ -270,6 +283,19 @@ class TemplateResponseTest(BaseTemplateResponseTest):
         for attr in template_attrs:
             with self.assertRaises(DiscardedAttributeError) as cm:
                 getattr(unpickled_response, attr)
+
+    def test_repickling(self):
+        response = SimpleTemplateResponse('first/test.html', {
+                'value': 123,
+                'fn': datetime.now,
+            })
+        self.assertRaises(ContentNotRenderedError,
+                          pickle.dumps, response)
+
+        response.render()
+        pickled_response = pickle.dumps(response)
+        unpickled_response = pickle.loads(pickled_response)
+        repickled_response = pickle.dumps(unpickled_response)
 
 
 class CustomURLConfTest(TestCase):
